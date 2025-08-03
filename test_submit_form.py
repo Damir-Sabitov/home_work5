2e23f16016914a47418393f36957d4ceb6b7f05a
Damir Sabitov <damirsabitov1998@gmail.com>
2025-08-04 00:03:24 +0300

    HW-5

--- test_submit_form.py
+++ test_submit_form.py
@@ -1,21 +1,27 @@
+import os, time
 from selene import browser, be, have
 
 def test_submit():
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'my_file.png'))
+
     browser.element('#firstName').type('John')
     browser.element('#lastName').type('Snow')
     browser.element('#userEmail').type('example@mail.ru')
     browser.element('label[for="gender-radio-1"]').should(be.clickable).click()
     browser.element('#userNumber').type('5555555555')
     browser.element('#subjectsInput').type('Hist').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
+
+    target = browser.element('[for="hobbies-checkbox-1"]')
+    browser.driver.execute_script("arguments[0].scrollIntoView(true);", target.locate())
+    target.should(be.visible).should(be.clickable).click()
+
     browser.element('#state').click()
     browser.element('#react-select-3-input').type('NCR').press_enter()
     browser.element('#city').click()
     browser.element('#react-select-4-input').type('Delhi').press_enter()
-    browser.element('#uploadPicture').send_keys('/Users/damirsabitov/PycharmProjects/home_work5/my_file.png')
+    browser.element('#uploadPicture').send_keys(file_path)
 
     browser.element('#submit').click()
-
 
     browser.element('.modal-content').should(be.visible)
 
@@ -26,4 +32,3 @@ def test_submit():
     browser.element('.table-responsive').should(have.text('History'))
     browser.element('.table-responsive').should(have.text('Sports'))
     browser.element('.table-responsive').should(have.text('my_file.png'))
-    browser.element('.table-responsive')
