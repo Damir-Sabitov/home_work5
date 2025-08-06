56ea3f5aba4e3fc9b72b601a668980e36cab5fe7
Damir Sabitov <damirsabitov1998@gmail.com>
2025-08-07 01:26:36 +0300

    добавил проверку полей адрес и дата рождения

--- test_submit_form.py
+++ test_submit_form.py
@@ -9,6 +9,16 @@ def test_submit():
     browser.element('#userEmail').type('example@mail.ru')
     browser.element('label[for="gender-radio-1"]').should(be.clickable).click()
     browser.element('#userNumber').type('5555555555')
+
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('.react-datepicker__month-select option[value="4"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('.react-datepicker__year-select option[value="1995"]').click()
+
+    browser.element('.react-datepicker__day--015:not(.react-datepicker__day--outside-month)').click()
+
+
     browser.element('#subjectsInput').type('Hist').press_enter()
 
     target = browser.element('[for="hobbies-checkbox-1"]')
@@ -20,6 +30,7 @@ def test_submit():
     browser.element('#city').click()
     browser.element('#react-select-4-input').type('Delhi').press_enter()
     browser.element('#uploadPicture').send_keys(file_path)
+    browser.element('#currentAddress').type('test street address')
 
     browser.element('#submit').click()
 
@@ -32,3 +43,6 @@ def test_submit():
     browser.element('.table-responsive').should(have.text('History'))
     browser.element('.table-responsive').should(have.text('Sports'))
     browser.element('.table-responsive').should(have.text('my_file.png'))
+    browser.element('.table-responsive').should(have.text('test street address'))
+    browser.element('.table-responsive').should(have.text('15 May,1995'))
+    browser.element('.table-responsive').should(have.text('NCR Delhi'))
